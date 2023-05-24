870af7b90b45a0049f6cdc151424052244095991
Ahmed Bassem <attta4122@gmail.com>
2023-05-24 16:37:43 +0300

    take input and validate data

--- lib/Views/Wights/custom_button_save.dart
+++ lib/Views/Wights/custom_button_save.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 class CustomButtonSave extends StatelessWidget {
-  const CustomButtonSave({Key? key}) : super(key: key);
-
+  const CustomButtonSave({Key? key, this.onTap}) : super(key: key);
+final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-        color: const Color(0xFF9A9A9A),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child:  Center(child: Text('Add',
-        style:  GoogleFonts.nunito(
-          color: const Color(0xFF252525),
-          fontSize: 18,
-          fontWeight: FontWeight.w600
-      ),
-      )
+    return  GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+          color: const Color(0xFF9A9A9A),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child:  Center(child: Text('Add',
+          style:  GoogleFonts.nunito(
+            color: const Color(0xFF252525),
+            fontSize: 18,
+            fontWeight: FontWeight.w600
+        ),
+        )
+        ),
       ),
     );
   }
--- lib/Views/Wights/custom_text_field.dart
+++ lib/Views/Wights/custom_text_field.dart
@@ -3,13 +3,21 @@ import 'package:google_fonts/google_fonts.dart';
 
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({Key? key, required this.hint,  this.maxLine =1}) : super(key: key);
+  const CustomTextField({Key? key, required this.hint,  this.maxLine =1, this.onSaved}) : super(key: key);
 
 final String hint;
 final int maxLine;
+final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value){
+        if(value?.isEmpty ?? true){
+          return 'Field is required';
+        }
+        return null;
+      },
       maxLines: maxLine,
       style: GoogleFonts.nunito(
 
