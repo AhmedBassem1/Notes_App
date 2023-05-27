d4905421fcbff276f02976262f780449d5248a40
Ahmed Bassem <attta4122@gmail.com>
2023-05-27 17:18:42 +0300

    Create fetch note model

--- lib/Views/notes_view.dart
+++ lib/Views/notes_view.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/Views/Wights/add_note_sheet.dart';
+import 'package:note_app/cubit/note%20cubit/note_cubit.dart';
 
 import 'Wights/notes_body_view.dart';
 
@@ -9,22 +10,26 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      floatingActionButton: FloatingActionButton(
-        hoverElevation: 20,
-        backgroundColor: const Color(0xFF252525),
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16)
-              ),
-              context: context, builder: (context){
-            return const AddNoteSheet();
-          });
-        },
-        child: const Icon(Icons.add_outlined,color: Colors.white,size: 35,),),
-      body:const NotesBodyViews() ,
+    return BlocProvider(
+      create: (context) => NoteCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          hoverElevation: 20,
+          backgroundColor: const Color(0xFF252525),
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)
+                ),
+                context: context, builder: (context) {
+              return const AddNoteSheet();
+            });
+          },
+          child: const Icon(
+            Icons.add_outlined, color: Colors.white, size: 35,),),
+        body: const NotesBodyViews(),
+      ),
     );
   }
 }
--- lib/cubit/note cubit/note_cubit.dart
+++ lib/cubit/note cubit/note_cubit.dart
@@ -1,10 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:hive/hive.dart';
 
 import 'package:note_app/Models/note_model.dart';
+
+import '../../Constants/constants.dart';
 
 part 'note_state.dart';
 
 class NoteCubit extends Cubit<NoteState> {
   NoteCubit() : super(NoteInitial());
+
+  fetchAllNote()async{
+    emit(NoteLoadingState());
+    try{
+      var noteBox=Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes=noteBox.values.toList();
+      emit(NoteSuccessState(notes));
+    }catch(e){
+      emit(NoteFieldState(e.toString()));
+    }
+  }
 }
